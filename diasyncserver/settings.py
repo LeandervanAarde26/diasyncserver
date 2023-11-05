76ba11dff11e2414a2f3c2b4782fda987aa54bd7
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-11-05 19:47:19 +0200

    .

--- diasyncserver/settings.py
+++ diasyncserver/settings.py
@@ -154,6 +154,7 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
