6c45521c735e95960cb4d2150888fd4c9c2a3bbf
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-11-05 19:26:32 +0200

    Added requirements

--- diasyncserver/settings.py
+++ diasyncserver/settings.py
@@ -12,19 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+from decouple import config
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z(ii+ncefuggmljm$ft57t61hlw^&y3=3_^ij5p0b(94#t!1n@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-# ALLOWED_HOSTS = [*]
-
+DEBUG = False
 
 # Application definition
 
