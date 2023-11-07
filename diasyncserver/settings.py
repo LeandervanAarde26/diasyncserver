d489d21fd1e31ff7ad563747c8184a35e679aacf
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-11-07 16:22:14 +0200

    Static issues

--- diasyncserver/settings.py
+++ diasyncserver/settings.py
@@ -156,7 +156,7 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
