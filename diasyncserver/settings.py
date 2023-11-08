35ed1d6f389f8c1d75f3382ece24b3eec16c1459
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-11-08 14:12:06 +0200

    ?

--- diasyncserver/settings.py
+++ diasyncserver/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000","https://diasync-leandervanaarde.vercel.app","https://diasync.vercel.app"]
+CORS_ALLOWED_ORIGINS = ["*"]
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 CORS_ALLOW_HEADERS = "*"
 
